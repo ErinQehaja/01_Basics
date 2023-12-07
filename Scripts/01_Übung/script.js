05b64423401c6c1ffd4c4069b6f88786ee320d7b
ErinQehaja <erinqehaja@hotmail.com>
2023-12-07 11:10:04 +0100

    01_Übung DONE

---
+++ Scripts/01_Übung/script.js
@@ -0,0 +1,14 @@
+console.log("hello");
+
+let a = 14;
+let h = 7;
+let j = 12;
+
+console.log("A hat den Wert " + a);
+
+let result = a + h +j; 
+console.log("Das Ergebnis ist" + result);
+
+console.log(a / h);
+
+console.log(h * j + a);
