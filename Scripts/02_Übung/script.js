f45d9a3f82fbd44589c0415ac776241eed856404
ErinQehaja <erinqehaja@hotmail.com>
2023-12-14 10:16:26 +0100

    02_Übung JS DONE

--- Scripts/02_Übung/script.js
+++ Scripts/02_Übung/script.js
@@ -4,3 +4,12 @@ console.log(name + " ist am schwätzen!");
 let a = 5
 let zustand = "richtig"
 console.log(a + " ist" ,zustand);
+
+let d = 17.4
+let e = 3.9
+let result = d / e
+console.log("The result of 17.4 divided by 3.9 is" + result);
+
+let firstname = "Erin"
+let lastname = "Qehaja"
+console.log("Ich heiße " + firstname + " " + lastname);
