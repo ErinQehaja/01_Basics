2f9986ccf1945209dd892f53c0b70191495e94e4
ErinQehaja <erinqehaja@hotmail.com>
2024-01-11 10:35:06 +0100

    05_Übung DONE

---
+++ Scripts/05_Übung/script.js
@@ -0,0 +1,9 @@
+let number = 9 
+
+if (number == 9) {
+    console.log("Die Zahl ist 9");
+} else if (number < 10) {
+    console.log("Die Zahl ist kleiner als 10");
+} else {
+    console.log("Die Zahl ist nicht 9 oder kleiner als 10");
+}
