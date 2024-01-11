79cc2c973ada9aede04f63e62408ca77cf1481e0
ErinQehaja <erinqehaja@hotmail.com>
2024-01-11 11:24:14 +0100

    08_Übung DONE

---
+++ Scripts/08_Übung/script.js
@@ -0,0 +1,29 @@
+
+let randomnumber = Math.random() * 6 + 5;
+let random = Math.floor(randomnumber);
+console.log(random);
+
+if (random == 10) {
+    console.log("Ten");
+}
+
+if (random == 9) {
+    console.log("Nine");
+}
+
+if (random == 8) {
+    console.log("Eight");
+}
+
+if (random == 7) {
+    console.log("Seven");
+}
+
+if (random == 6) {
+    console.log("Six");
+}
+
+if (random == 5) {
+    console.log("Five");
+}
+
