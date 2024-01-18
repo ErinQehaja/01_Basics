8d3caba77b5849d5bea44056fe79f95789861fcf
ErinQehaja <erinqehaja@hotmail.com>
2024-01-18 11:25:53 +0100

    09_Übung LESSON END

---
+++ Scripts/09_Übung/script.js
@@ -0,0 +1,45 @@
+let output = "";
+for(let index = 1; index <=4; index++) {
+    output += "X ";
+    if(index%4==0) {
+        console.log(output);
+        output = ""
+    }
+}
+
+output=""
+for(let index = 1; index <=4; index++) {
+    output += "0 ";
+    if(index%4==0) {
+        console.log(output);
+        output = ""
+    }
+}
+
+
+output = "";
+for(let index = 1; index <=4; index++) {
+    output += "X ";
+    if(index%4==0) {
+        console.log(output);
+        output = ""
+    }
+}
+
+
+output="";
+for(let index = 1; index <=4; index++) {
+    output += "0 ";
+    if(index%4==0) {
+        console.log(output);
+        output = ""
+    }
+}
+
+for(let a = 1; a <=100; a++) {
+    console.log(a);
+}
+
+
+
+
