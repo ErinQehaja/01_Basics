f85c704fcc05408e9680625b9f5cb30528ef76bb
ErinQehaja <erinqehaja@hotmail.com>
2024-03-07 11:06:23 +0100

    TikTakToe DONE

--- Scripts/14_Übung/scripts14.1.js
+++ Scripts/14_Übung/scripts14.1.js
@@ -1,7 +1,7 @@
 let field = [
-    [1,2,3],
-    [4,5,6],
-    [7,8,9]
+    [1, 2, 2],
+    [2, 2, 1],
+    [1, 1, 2]
 ];
 
 for (let row = 0; row < field.length; row++) {
@@ -9,10 +9,59 @@ for (let row = 0; row < field.length; row++) {
     let output = "";
     for (let col = 0; col < currentRow.length; col++) {
         output += currentRow[col] + " ";
-        
-    } 
+    }
     console.log(output);
 }
-    
+
+let winner = null;
+
+// Überprüfe, ob Spieler 1 gewonnen hat
+for (let row = 0; row < field.length; row++) {
+    if (field[row][0] === 1 && field[row][1] === 1 && field[row][2] === 1) {
+        winner = 1;
+        break;
+    }
+}
+
+for (let col = 0; col < field[0].length; col++) {
+    if (field[0][col] === 1 && field[1][col] === 1 && field[2][col] === 1) {
+        winner = 1;
+        break;
+    }
+}
+
+if ((field[0][0] === 1 && field[1][1] === 1 && field[2][2] === 1) ||
+    (field[0][2] === 1 && field[1][1] === 1 && field[2][0] === 1)) {
+    winner = 1;
+}
+
+// Überprüfe, ob Spieler 2 gewonnen hat
+if (!winner) {
+    for (let row = 0; row < field.length; row++) {
+        if (field[row][0] === 2 && field[row][1] === 2 && field[row][2] === 2) {
+            winner = 2;
+            break;
+        }
+    }
+
+    for (let col = 0; col < field[0].length; col++) {
+        if (field[0][col] === 2 && field[1][col] === 2 && field[2][col] === 2) {
+            winner = 2;
+            break;
+        }
+    }
+
+    if ((field[0][0] === 2 && field[1][1] === 2 && field[2][2] === 2) ||
+        (field[0][2] === 2 && field[1][1] === 2 && field[2][0] === 2)) {
+        winner = 2;
+    }
+}
+
+// Gib den Gewinner aus oder das Feld, wenn es keinen Gewinner gibt
+if (winner) {
+    console.log("Player " + winner + " is the winner");
+} else {
+    console.log("It is a tie!");
+}
 
 
