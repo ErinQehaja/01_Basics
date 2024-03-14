5e6e9d26cca4e21910c327cd092f6319c78e6275
ErinQehaja <erinqehaja@hotmail.com>
2024-03-14 19:53:39 +0100

    Rechner PlusShown

--- HTML_JS/01_Übung/script.js
+++ HTML_JS/01_Übung/script.js
@@ -3,11 +3,12 @@ function buttonPlus() {
     let div = document.getElementById('sign');
     var character = '+';
     div.innerHTML += character;
+
+
 }
 
 
 function buttonClick() {
-    console.log("click");
     let num1 = document.getElementById("number1").value;
     let num2 = document.getElementById("number2").value;
 
