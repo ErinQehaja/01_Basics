0f2b29f7df6628daf26ab6749c084df77ccf94b7
ErinQehaja <erinqehaja@hotmail.com>
2024-03-14 11:28:36 +0100

    Rechner LessonEND

--- HTML_JS/01_Übung/script.js
+++ HTML_JS/01_Übung/script.js
@@ -1,1 +1,17 @@
-document.getElementById("content").innerHTML = "Tolle Sache";
+
+function buttonPlus() {
+    let div = document.getElementById('sign');
+    var character = '+';
+    div.innerHTML += character;
+}
+
+
+function buttonClick() {
+    console.log("click");
+    let num1 = document.getElementById("number1").value;
+    let num2 = document.getElementById("number2").value;
+
+    let result = parseFloat(num1) + parseFloat(num2);
+    console.log(result);
+}
+
