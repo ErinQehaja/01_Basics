2b3ae1780a37803f65dcd31c8d49c2462abea8c2
ErinQehaja <erinqehaja@hotmail.com>
2024-04-04 10:26:52 +0200

    01_MINI RECHNER DONE

--- HTML_JS/01_Übung/script.js
+++ HTML_JS/01_Übung/script.js
@@ -1,18 +1,55 @@
+let currentOperation = '+';
 
 function buttonPlus() {
     let div = document.getElementById('sign');
     var character = '+';
-    div.innerHTML += character;
-
-
+    div.textContent = character;
+    currentOperation = character;
 }
 
+function buttonMinus() {
+    let div = document.getElementById('sign');
+    var character = '-';
+    div.textContent = character;
+    currentOperation = character;
+}
+
+function buttonMultiply() {
+    let div = document.getElementById('sign');
+    var character = '*';
+    div.textContent = character;
+    currentOperation = character;
+}
+
+function buttonDivide() {
+    let div = document.getElementById('sign');
+    var character = '/';
+    div.textContent = character;
+    currentOperation = character;
+}
 
 function buttonClick() {
     let num1 = document.getElementById("number1").value;
     let num2 = document.getElementById("number2").value;
 
-    let result = parseFloat(num1) + parseFloat(num2);
+    let result;
+
+    switch (currentOperation) {
+        case '+':
+            result = parseFloat(num1) + parseFloat(num2);
+            break;
+        case '-':
+            result = parseFloat(num1) - parseFloat(num2);
+            break;
+        case '*':
+            result = parseFloat(num1) * parseFloat(num2);
+            break;
+        case '/':
+            result = parseFloat(num1) / parseFloat(num2);
+            break;
+        default:
+            result = 'Invalid Operation';
+    }
     console.log(result);
 }
 
