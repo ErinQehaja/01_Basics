50d7dd361d56de45fae52692c54826922171e649
ErinQehaja <erinqehaja@hotmail.com>
2024-04-18 10:23:00 +0200

    Test_18.4.24

---
+++ Scripts/Test_18.4.24/script.js
@@ -0,0 +1,11 @@
+const data = "Hans geht heute schwimmen";
+
+let arr = ["schwimmen", "geht", "heute", "Hans"];
+
+for (let index = 0; index < arr.length; index++) {
+    console.log(arr[index]);
+}
+
+const numbers = "1,2,1,4,6,8,7";
+
+let 
